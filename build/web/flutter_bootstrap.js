836836f1172bd56151029f3934df46a34ca74b64
EsmeMejia29 <esmejia2908@gmail.com>
2026-06-04 21:30:59 -0600

    build: actualizar compilacion release web con soporte de audio y fuentes

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "932377528" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2453796055" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
