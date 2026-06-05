58b4decd1e56153c12dbc9b953f3c6957bbc1e13
EsmeMejia29 <esmejia2908@gmail.com>
2026-06-04 21:20:11 -0600

    style: actualizar release de produccion web para el calendario

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2566899190" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "932377528" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
